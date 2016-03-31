e960a75bfc4a4c1b44aa6b3aa15e9be6fc134235
lauraSR <laura_sanz92@hotmail.com>
2016-03-31 17:08:58 +0200

    He realizado el ejercicio

--- calc/views.py
+++ calc/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def suma(request, op1, op2):
+    suma = int(op1) + int(op2)
+return HttpResponse("Resultado: " op1 + " + " + op2 + " = " + str(suma))
+
+def resta(request, op1, op2):
+    resta = int(op1) - int(op2)
+    return HttpResponse("Resultado: " op1 + " - " + op2 + " = " + str(resta))
+
+
+def multi(request, op1, op2):
+    multi = int(op) * int(op)
+    return HttpResponse("Resultado: " op + " * " + op2 + " = " + str(multi))
+
+def divi(request, op1, op2):
+    try:
+        divi = int(op1) / int(op2)
+    except ZeroDivisionError:
+        return HttpResponse("No puedes dividir entre 0")
+
+    return HttpResponse("Resultado: " op1 + " / " + op2 + " = " + str(divi))
+
+def error(request):
+    return HttpResponse('Introduzca la operación que quiere realizar')
